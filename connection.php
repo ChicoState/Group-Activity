ac397af134b9cc3fdfd7f9b678a01e157e6382b7
ANorquist <89611484+AustinNorquist@users.noreply.github.com>
2023-05-14 19:08:06 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php
 
-//$con = mysqli_connect("localhost", "csci430", "jagpal08", "chicoSchool");
-$con = mysqli_connect("localhost", "csci430", "jagpal08", "chicoSchool");
+$con = mysqli_connect("", "", "", "");
 
 if ($con->connect_error) {
   die("Connection failed: " . $con->connect_error);
